e5c4f31349ca278b3bd8bac4e246f9ed897a92b6
SantiParris8 <32781893+SantiParris8@users.noreply.github.com>
2017-10-13 19:10:09 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function masinfo() {
+  $(".jumbotron").animate({height: '500px'});
+  $('.button').hide();
+  $(".lead").text("Soy de Montevideo Uruguay, tengo 16 años, disfruto el mundo de la computación y la tecnología. En mi tiempo libre me gusta intentar diseñar sitios web y aprender edición digital junto con programación. Mi programa favorito para editar imagenes es Photoshop y para crear contenido suelo utilizar Illustrator. Tengo un dominio del ingles avanzado y deberia dar el examen de cambridge PROF este año (2017).");
+  $('.segundo').fadeIn();
+
+};
+
+$('document').ready(function(){
+
+    $("#cita1").animate({marginRight: '25%'},1000);
+    $("#cita2").animate({marginLeft: '30%'},1200);
+    $("#cita3").animate({marginLeft: '10%'},1400);
+    $("#cita4").animate({marginLeft: '30%'},1200);
+
+
+
+});
